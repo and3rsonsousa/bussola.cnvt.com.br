50c335f979bea153ee9030f3ad1b6fe37ce756dc
Chico Sousa <ochicosousa@gmail.com>
2025-01-24 15:07:31 -0300

    object sans

--- app/routes.ts
+++ app/routes.ts
@@ -1,36 +1,38 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-	index("routes/home.tsx"),
-	route("dashboard", "routes/dashboard.tsx", [
-		index("./routes/dashboard.home.tsx"),
-		route(":partner", "routes/dashboard.partner.tsx"),
-		route(":partner/late", "routes/dashboard.partner.late.tsx"),
-		route(":partner/archived", "routes/dashboard.partner.archived.tsx"),
-		route("action/:id", "routes/dashboard.action.id.tsx"),
+  index("routes/home.tsx"),
+  route("dashboard", "routes/dashboard.tsx", [
+    index("./routes/dashboard.home.tsx"),
+    route(":partner", "routes/dashboard.partner.tsx"),
+    route(":partner/late", "routes/dashboard.partner.late.tsx"),
+    route(":partner/archived", "routes/dashboard.partner.archived.tsx"),
+    route("action/:id", "routes/dashboard.action.id.tsx"),
 
-		route("admin/users", "routes/dashboard.admin.users.tsx"),
-		route(
-			"admin/user/:id/actions",
-			"routes/dashboard.admin.user.actions.tsx"
-		),
-		route("admin/user/:id", "routes/dashboard.admin.user.tsx"),
+    route("admin/users", "routes/dashboard.admin.users.tsx"),
+    route("admin/user/:id/actions", "routes/dashboard.admin.user.actions.tsx"),
+    route("admin/user/:id", "routes/dashboard.admin.user.tsx"),
 
-		route("admin/partners", "routes/dashboard.admin.partners.tsx"),
-		route("admin/partner/:id", "routes/dashboard.admin.partner.id.tsx"),
-		route("admin/partner/new", "routes/dashboard.admin.partner.new.tsx"),
+    route("admin/partners", "routes/dashboard.admin.partners.tsx"),
+    route("admin/partner/:id", "routes/dashboard.admin.partner.id.tsx"),
+    route("admin/partner/new", "routes/dashboard.admin.partner.new.tsx"),
 
-		route("me", "routes/dashboard.me.tsx"),
-		route("help", "routes/dashboard.help.tsx"),
-	]),
+    route("me", "routes/dashboard.me.tsx"),
+    route("help", "routes/dashboard.help.tsx"),
+  ]),
 
-	route("login", "routes/login.tsx"),
-	route("logout", "routes/logout.tsx"),
+  route("login", "routes/login.tsx"),
+  route("logout", "routes/logout.tsx"),
 
-	route("handle-openai", "routes/handle-openai.tsx"),
-	route("handle-actions", "routes/handle-actions.tsx"),
+  route("handle-openai", "routes/handle-openai.tsx"),
+  route("handle-actions", "routes/handle-actions.tsx"),
 
-	route("ui", "routes/ui.tsx"),
-	route("set-theme", "routes/set-theme.tsx"),
-	route("report/:partner", "routes/report.partner.tsx"),
+  route("ui", "routes/ui.tsx"),
+  route("set-theme", "routes/set-theme.tsx"),
+  route("report/:partner", "routes/report.partner.tsx"),
+
+  route(
+    "/object-sans/subset-PPObjectSans-Bold.woff",
+    "/object-sans/subset-PPObjectSans-Bold.woff",
+  ),
 ] satisfies RouteConfig;
