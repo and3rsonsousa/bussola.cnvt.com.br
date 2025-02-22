2ff5d3c1693fc02d514c3a39f12125e23b97671b
Chico Sousa <ochicosousa@gmail.com>
2025-02-22 10:57:45 -0300

    archive

--- app/routes/dashboard.admin.partners.tsx
+++ app/routes/dashboard.admin.partners.tsx
@@ -1,13 +1,14 @@
-import { ArchiveIcon, Trash2Icon } from "lucide-react";
+import { ArchiveIcon, ArchiveRestoreIcon, Trash2Icon } from "lucide-react";
 import {
-  Form,
   Link,
   useLoaderData,
   useMatches,
+  useSubmit,
   type LoaderFunctionArgs,
   type MetaFunction,
 } from "react-router";
 import { Heading } from "~/components/Headings";
+import { INTENTS } from "~/lib/constants";
 import { Avatar, AvatarGroup, getResponsibles } from "~/lib/helpers";
 import { createClient } from "~/lib/supabase";
 
@@ -39,77 +40,129 @@ export const meta: MetaFunction = () => {
 };
 
 export default function AdminPartners() {
-  const { partners } = useLoaderData<typeof loader>();
-
   const matches = useMatches();
+
+  let { partners } = useLoaderData<typeof loader>();
+  let partnersA: Partner[] = [];
+  let partnersB: Partner[] = [];
+  partners?.map((p) => {
+    if (p.archived) {
+      partnersB.push(p);
+    } else {
+      partnersA.push(p);
+    }
+  });
   const { people } = matches[1].data as DashboardRootType;
 
   return (
     <div className="scrollbars-v">
-      <div className="px-4 py-4 lg:px-8">
+      <div className="px-4 py-12 lg:px-8">
         <Heading>Parceiros</Heading>
         <div className="grid items-center gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          {partners?.map((partner: Partner) => (
-            <Link
-              to={`/dashboard/admin/partner/${partner.slug}`}
-              className="group bg-card ring-ring hover:bg-popover relative z-1 flex justify-between overflow-hidden rounded-xl p-6 tracking-tight shadow-sm transition-[background-color,box-shadow] duration-500 select-none focus-within:ring-2 hover:z-10 hover:shadow-2xl"
-              key={partner.slug}
-              tabIndex={-1}
-            >
-              <div className="flex w-full gap-4">
-                <Avatar
-                  item={{
-                    short: partner.short,
-                    bg: partner.colors[0],
-                    fg: partner.colors[1],
-                  }}
-                  size="lg"
-                />
-                <div className="w-full overflow-hidden">
-                  <div className="mb-1 flex w-[90%] gap-2 overflow-hidden text-2xl font-bold outline-hidden">
-                    <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-                      {partner.title}
-                    </div>
-                    {partner.archived && <ArchiveIcon className="opacity-25" />}
-                  </div>
-                  <div className="text-xs font-medium tracking-wide opacity-50">
-                    {partner.slug}
-                  </div>
-                  <div className="mt-4 flex w-full items-center justify-between gap-4">
-                    <div>
-                      <AvatarGroup
-                        size="sm"
-                        avatars={getResponsibles(people, partner.users_ids).map(
-                          (r) => ({
-                            item: {
-                              image: r.image,
-                              short: r.initials!,
-                              title: r.name,
-                            },
-                          }),
-                        )}
-                      />
-                    </div>
-                    <div className="flex items-center justify-end gap-1 text-[10px] font-bold tracking-wide uppercase opacity-50">
-                      {partner.sow === "marketing"
-                        ? "Consultoria de Marketing 360"
-                        : partner.sow === "socialmedia"
-                          ? "Gestão de Redes Sociais"
-                          : "Serviços avulsos"}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <Form className="absolute top-4 right-4">
-                <input name="id" value={partner.id} type="hidden" />
-                <button className="translate-x-12 transition-transform duration-500 group-hover:translate-x-0">
-                  <Trash2Icon className="h-6 w-6 opacity-75" />
-                </button>
-              </Form>
-            </Link>
+          {partnersA?.map((partner: Partner) => (
+            <PartnerLink partner={partner} people={people} />
+          ))}
+        </div>
+        <h3 className="mt-8 mb-4 text-2xl font-bold uppercase">Arquivados</h3>
+        <div className="grid items-center gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {partnersB?.map((partner: Partner) => (
+            <PartnerLink partner={partner} people={people} />
           ))}
         </div>
       </div>
     </div>
+  );
+}
+
+function PartnerLink({
+  partner,
+  people,
+}: {
+  partner: Partner;
+  people: Person[];
+}) {
+  const submit = useSubmit();
+
+  return (
+    <Link
+      to={`/dashboard/admin/partner/${partner.slug}`}
+      className="group bg-card ring-ring hover:bg-popover relative z-1 flex justify-between overflow-hidden rounded-xl p-6 tracking-tight shadow-sm transition-[background-color,box-shadow] duration-500 select-none focus-within:ring-2 hover:z-10 hover:shadow-2xl"
+      key={partner.slug}
+      tabIndex={-1}
+    >
+      <div className="flex w-full gap-4">
+        <Avatar
+          item={{
+            short: partner.short,
+            bg: partner.colors[0],
+            fg: partner.colors[1],
+          }}
+          size="lg"
+        />
+        <div className="w-full overflow-hidden">
+          <div className="mb-1 flex w-[90%] gap-2 overflow-hidden text-2xl font-bold outline-hidden">
+            <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+              {partner.title}
+            </div>
+            {/* {partner.archived && <ArchiveIcon className="opacity-25" />} */}
+          </div>
+          <div className="text-xs font-medium tracking-wide opacity-50">
+            {partner.slug}
+          </div>
+          <div className="mt-4 flex w-full items-center justify-between gap-4">
+            <div>
+              <AvatarGroup
+                size="sm"
+                avatars={getResponsibles(people, partner.users_ids).map(
+                  (r) => ({
+                    item: {
+                      image: r.image,
+                      short: r.initials!,
+                      title: r.name,
+                    },
+                  }),
+                )}
+              />
+            </div>
+            <div className="flex items-center justify-end gap-1 text-[10px] font-bold tracking-wide uppercase opacity-50">
+              {partner.sow === "marketing"
+                ? "Consultoria de Marketing 360"
+                : partner.sow === "socialmedia"
+                  ? "Gestão de Redes Sociais"
+                  : "Serviços avulsos"}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-4 right-4">
+        <button
+          className="translate-x-12 transition-transform duration-500 group-hover:translate-x-0"
+          onClick={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("Archive");
+            submit(
+              {
+                intent: INTENTS.updatePartner,
+                archived: !partner.archived,
+                id: partner.id,
+              },
+              {
+                action: "/handle-actions",
+                method: "post",
+                navigate: false,
+              },
+            );
+            return false;
+          }}
+        >
+          {partner.archived ? (
+            <ArchiveRestoreIcon className="h-6 w-6 opacity-75" />
+          ) : (
+            <ArchiveIcon className="h-6 w-6 opacity-75" />
+          )}
+        </button>
+      </div>
+    </Link>
   );
 }
--- app/routes/handle-actions.tsx
+++ app/routes/handle-actions.tsx
@@ -5,190 +5,189 @@ import { createClient } from "~/lib/supabase";
 export const config = { runtime: "edge" };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	const { supabase } = createClient(request);
+  const { supabase } = createClient(request);
 
-	const formData = await request.formData();
-	const { intent, id, ...values } = Object.fromEntries(formData.entries());
+  const formData = await request.formData();
+  let { intent, id, ...values } = Object.fromEntries(formData.entries());
 
-	if (!intent) throw new Error("No intent was defined");
+  if (id) id = id.toString();
 
-	if (intent === INTENTS.createAction) {
-		const actionToInsert = {
-			id: id.toString(),
-			created_at: values["created_at"].toString(),
-			updated_at: values["updated_at"].toString(),
+  if (!intent) throw new Error("No intent was defined");
 
-			category: values["category"].toString(),
-			state: values["state"].toString(),
+  if (intent === INTENTS.createAction) {
+    const actionToInsert = {
+      id: id.toString(),
+      created_at: values["created_at"].toString(),
+      updated_at: values["updated_at"].toString(),
 
-			priority: PRIORITIES.medium,
-			date: values["date"].toString(),
-			instagram_date: values["instagram_date"].toString(),
-			time:
-				Number(values["time"]) ||
-				(TIMES as any)[values["category"].toString()],
+      category: values["category"].toString(),
+      state: values["state"].toString(),
 
-			description: values["description"].toString(),
-			title: values["title"].toString(),
-			responsibles: values["responsibles"].toString().split(","),
-			partners: values["partners"].toString().split(","),
-			user_id: values["user_id"].toString(),
-			color: values["color"].toString(),
-			caption: "",
-		};
+      priority: PRIORITIES.medium,
+      date: values["date"].toString(),
+      instagram_date: values["instagram_date"].toString(),
+      time:
+        Number(values["time"]) || (TIMES as any)[values["category"].toString()],
 
-		const { data, error } = await supabase
-			.from("actions")
-			.insert(actionToInsert)
-			.select()
-			.single();
-		if (error) console.log({ error });
+      description: values["description"].toString(),
+      title: values["title"].toString(),
+      responsibles: values["responsibles"].toString().split(","),
+      partners: values["partners"].toString().split(","),
+      user_id: values["user_id"].toString(),
+      color: values["color"].toString(),
+      caption: "",
+    };
 
-		return { data, error };
-	} else if (intent === INTENTS.updateAction) {
-		if (!id) throw new Error("No id was provided");
+    const { data, error } = await supabase
+      .from("actions")
+      .insert(actionToInsert)
+      .select()
+      .single();
+    if (error) console.log({ error });
 
-		// delete values.priority;
-		// delete values.category;
-		// delete values.state;
-		// delete values.partner;
-		// delete values.slug;
-		delete values.archived;
+    return { data, error };
+  } else if (intent === INTENTS.updateAction) {
+    if (!id) throw new Error("No id was provided");
 
-		if (values.color === "" || values.color === null) delete values.color;
+    // delete values.priority;
+    // delete values.category;
+    // delete values.state;
+    // delete values.partner;
+    // delete values.slug;
+    delete values.archived;
 
-		if (values["files"] !== "null") {
-			//@ts-ignore
-			values["files"] = values["files"].toString().split(",");
-		} else {
-			//@ts-ignore
-			values["files"] = null;
-		}
-		if (values["responsibles"] !== "null") {
-			//@ts-ignore
-			values["responsibles"] = values["responsibles"]
-				.toString()
-				.split(",");
-		}
+    if (values.color === "" || values.color === null) delete values.color;
 
-		//@ts-ignore
-		values["partners"] = values["partners"].toString().split(",");
+    if (values["files"] !== "null") {
+      //@ts-ignore
+      values["files"] = values["files"].toString().split(",");
+    } else {
+      //@ts-ignore
+      values["files"] = null;
+    }
+    if (values["responsibles"] !== "null") {
+      //@ts-ignore
+      values["responsibles"] = values["responsibles"].toString().split(",");
+    }
 
-		const { data, error } = await supabase
-			.from("actions")
-			.update({
-				...values,
-			})
-			.eq("id", id);
+    //@ts-ignore
+    values["partners"] = values["partners"].toString().split(",");
 
-		if (error) console.log({ from: "UPDATE ACTION", error });
+    const { data, error } = await supabase
+      .from("actions")
+      .update({
+        ...values,
+      })
+      .eq("id", id);
 
-		return { data, error };
-	} else if (intent === INTENTS.duplicateAction) {
-		const { data: oldAction } = await supabase
-			.from("actions")
-			.select("*")
-			.eq("id", id)
-			.single();
-		if (oldAction) {
-			const newId = values["newId"].toString();
-			const created_at = values["created_at"].toString();
-			const updated_at = values["updated_at"].toString();
-			const { data: newAction, error } = await supabase
-				.from("actions")
-				.insert({
-					...oldAction,
-					id: newId,
-					created_at,
-					updated_at,
-				})
-				.select()
-				.single();
+    if (error) console.log({ from: "UPDATE ACTION", error });
 
-			return { newAction, error };
-		}
-	} else if (intent === INTENTS.deleteAction) {
-		const data = await supabase
-			.from("actions")
-			.update({ archived: true })
-			.eq("id", id);
+    return { data, error };
+  } else if (intent === INTENTS.duplicateAction) {
+    const { data: oldAction } = await supabase
+      .from("actions")
+      .select("*")
+      .eq("id", id)
+      .single();
+    if (oldAction) {
+      const newId = values["newId"].toString();
+      const created_at = values["created_at"].toString();
+      const updated_at = values["updated_at"].toString();
+      const { data: newAction, error } = await supabase
+        .from("actions")
+        .insert({
+          ...oldAction,
+          id: newId,
+          created_at,
+          updated_at,
+        })
+        .select()
+        .single();
 
-		return { data };
-	} else if (intent === INTENTS.recoverAction) {
-		const data = await supabase
-			.from("actions")
-			.update({ archived: false })
-			.eq("id", id);
+      return { newAction, error };
+    }
+  } else if (intent === INTENTS.deleteAction) {
+    const data = await supabase
+      .from("actions")
+      .update({ archived: true })
+      .eq("id", id);
 
-		return { data };
-	} else if (intent === INTENTS.destroyAction) {
-		const data = await supabase.from("actions").delete().eq("id", id);
+    return { data };
+  } else if (intent === INTENTS.recoverAction) {
+    const data = await supabase
+      .from("actions")
+      .update({ archived: false })
+      .eq("id", id);
 
-		return { data };
-	} else if (intent === INTENTS.updatePerson) {
-		if (!id) throw new Error("No id was provided");
-		const { data, error } = await supabase
-			.from("people")
-			.update({
-				...values,
-			})
-			.eq("id", id);
+    return { data };
+  } else if (intent === INTENTS.destroyAction) {
+    const data = await supabase.from("actions").delete().eq("id", id);
 
-		if (error) console.log({ error });
+    return { data };
+  } else if (intent === INTENTS.updatePerson) {
+    if (!id) throw new Error("No id was provided");
+    const { data, error } = await supabase
+      .from("people")
+      .update({
+        ...values,
+      })
+      .eq("id", id);
 
-		return { data, error };
-	} else if (intent === INTENTS.updatePartner) {
-		if (!id) throw new Error("No id was provided");
+    if (error) console.log({ error });
 
-		if (values.users_ids) {
-			//@ts-ignore
-			values["users_ids"] = String(values["users_ids"]).split(",");
-		}
+    return { data, error };
+  } else if (intent === INTENTS.updatePartner) {
+    if (!id) throw new Error("No id was provided");
 
-		const { data, error } = await supabase
-			.from("partners")
-			.update({
-				...values,
-			})
-			.eq("id", id);
+    if (values.users_ids) {
+      //@ts-ignore
+      values["users_ids"] = String(values["users_ids"]).split(",");
+    }
 
-		if (error) console.log({ error });
+    const { data, error } = await supabase
+      .from("partners")
+      .update({
+        ...values,
+      })
+      .eq("id", id);
 
-		return { data, error };
-	} else if (intent === INTENTS.setSprint) {
-		const sprint = {
-			id: id.toString(),
-			created_at: values["created_at"].toString(),
-			action_id: values["action_id"].toString(),
-			user_id: values["user_id"].toString(),
-		};
+    if (error) console.log({ error });
 
-		const { data, error } = await supabase
-			.from("sprints")
-			.insert({ ...sprint })
-			.select()
-			.single();
+    return { data, error };
+  } else if (intent === INTENTS.setSprint) {
+    const sprint = {
+      id: id.toString(),
+      created_at: values["created_at"].toString(),
+      action_id: values["action_id"].toString(),
+      user_id: values["user_id"].toString(),
+    };
 
-		if (error) console.log({ error });
+    const { data, error } = await supabase
+      .from("sprints")
+      .insert({ ...sprint })
+      .select()
+      .single();
 
-		return { data, error };
-	} else if (intent === INTENTS.unsetSprint) {
-		const sprint = {
-			action_id: values["action_id"].toString(),
-			user_id: values["user_id"].toString(),
-		};
+    if (error) console.log({ error });
 
-		const { data, error } = await supabase
-			.from("sprints")
-			.delete()
-			.match(sprint)
-			.select()
-			.single();
+    return { data, error };
+  } else if (intent === INTENTS.unsetSprint) {
+    const sprint = {
+      action_id: values["action_id"].toString(),
+      user_id: values["user_id"].toString(),
+    };
 
-		if (error) console.log({ error });
+    const { data, error } = await supabase
+      .from("sprints")
+      .delete()
+      .match(sprint)
+      .select()
+      .single();
 
-		return { data, error };
-	}
+    if (error) console.log({ error });
 
-	return {};
+    return { data, error };
+  }
+
+  return {};
 };
